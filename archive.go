2a1fe58c24c203b204025915a87e59c259fc969f
Paul Tagliamonte <tag@pault.ag>
2016-07-01 09:19:42 -0400

    add includedeb

--- archive.go
+++ archive.go
@@ -1,6 +1,8 @@
 package inept
 
 import (
+	"path"
+
 	"pault.ag/go/archive"
 	"pault.ag/go/debian/deb"
 
@@ -20,10 +22,21 @@ func NewRepository(db *gorm.DB, arch *archive.Archive) (*Repository, error) {
 }
 
 func (repo Repository) IncludeDeb(debFile *deb.Deb) error {
-	_, _, err := repo.Archive.Pool.IncludeDeb(debFile)
+	debPath, obj, err := repo.Archive.Pool.IncludeDeb(debFile)
 	if err != nil {
 		return err
 	}
+
+	fd, err := repo.Archive.Open(*obj)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+	debFile, err = deb.Load(fd, path.Join(repo.Archive.Path(), debPath))
+	if err != nil {
+		return err
+	}
+
 	return IndexDeb(repo.DB, *repo.Archive, []string{
 		"sha1", "sha256", "sha512",
 	}, debFile)
